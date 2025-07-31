09d2e80f27a6035314f98193c8cfb505cfbf15eb
hechaogm <hechaogm@gmail.com>
2025-07-31 15:26:38 +0800

    ESP32-S3-OV2640-MP

---
+++ esp32-s3-CAM-MP-OV2640/scripts/auth.py
@@ -0,0 +1,12 @@
+
+# connect to access point
+AP = const('111')     # Your SSID
+PW = const('electrodragon')     # Your password
+
+
+# WEB user - can change to whatever
+UID = const('111')          # authentication user
+PWD = const('electrodragon')    # authentication password
+# URL with the above UID and PWD, eg
+# http://192.168.4.74/free/Hi-Nove-La
+
